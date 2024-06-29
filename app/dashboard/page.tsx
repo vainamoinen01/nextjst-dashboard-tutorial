e9fa567c4bde95dacda92d1909fd0d5c68dec28b
Vainamoinen <benjamimdps@gmail.com>
2024-06-29 12:01:05 -0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
 
-export default function Page() {
+export default async function Page() {
     const revenue = await fetchRevenue();
     return (
         <main>
@@ -22,7 +22,7 @@ export default function Page() {
                 /> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                {<RevenueChart revenue={revenue} />}
+                <RevenueChart revenue={revenue} />
                 {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
             </div>
         </main>
